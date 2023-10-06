740f2b6725e3a961a4b3b445514c3f464f612cd1
aistart <aistart@aliyun.com>
2023-10-06 20:26:27 +0800

    daooo.top

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const DAOOOTOP_URL = `https://www.daooo.top`;
+export const DAOOOTOP_URL = `https://daooo.top`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
